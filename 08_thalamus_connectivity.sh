fecb2cda2f762dfd07399f43d34b4c6334db1221
bart-larsen <bart.larsen@pennmedicine.upenn.edu>
2020-12-13 11:21:45 -0500

    renaming

--- 08_thalamus_connectivity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sublist="/cbica/projects/alpraz_EI/input/n1601_sublist.csv" 
-
-while IFS=, read subid sesid other; do
-	call="qsub -l short -N "thalamus_$subid_$sesid" proc_one_thalamus.sh $subid $sesid"
-	eval $call
-
-done <$sublist
--- 09_1_mvm_prep.sh
+++
@@ -1,1 +0,0 @@
-fat_mat_tableize.py -prefix '../input/schaef200vars' -in_csv '../input/PNC_sublist_FD_age_distance_filter.csv' -in_mat ../data/PNC/IDEMO_ACOMPCOR_GSR/*/*/schaefer200x7_aal_wholebrain_CC_000.netcc
